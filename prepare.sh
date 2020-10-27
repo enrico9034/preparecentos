cd8a541cfdb09d5312079571dfec979d2a8e1d45
enrico9034 <enrico9034@gmail.com>
2020-10-27 12:24:04 +0100

    Create prepare.sh

---
+++ prepare.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+yum-config-manager --enable rhui-REGION-rhel-server-extras
+yum update
+yum -y install docker
+chkconfig docker on
+sed -i -e 's/slave/shared/g' /lib/systemd/system/docker.service
+systemctl daemon-reload
+service docker start
+useradd dockeruser
+usermod -aG docker dockeruser
+mkdir /opt/cni /etc/cni
+chcon -Rt svirt_sandbox_file_t /etc/cni
+chcon -Rt svirt_sandbox_file_t /opt/cni
