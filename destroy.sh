25a3667032119fa6fff5b261bfd528d772d16edc
enrico9034 <enrico9034@gmail.com>
2020-11-16 16:01:38 +0100

    Update destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -1,8 +1,8 @@
 for mount in $(mount | grep tmpfs | grep '/var/lib/kubelet' | awk '{​ print $3 }​') /var/lib/kubelet /var/lib/rancher; do umount $mount; done
-docker stop $(docker ps -aq)
-docker image rm $(docker image ls -q)
-docker rm -f $(docker ps -qa)
-docker volume rm $(docker volume ls -q)
+docker stop $(docker ps -aq) --force
+docker image rm $(docker image ls -q) --force
+docker rm -f $(docker ps -qa) --force
+docker volume rm $(docker volume ls -q) --force
 cleanupdirs="/etc/ceph /etc/cni /etc/kubernetes /opt/cni /opt/rke /run/secrets/kubernetes.io /run/calico /run/flannel /var/lib/calico /var/lib/etcd /var/lib/cni /var/lib/kubelet /var/lib/rancher/rke/log /var/log/containers /var/log/kube-audit /var/log/pods /var/run/calico"
 for dir in $cleanupdirs; do
   echo "Removing $dir"
